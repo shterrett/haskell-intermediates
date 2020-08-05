317d72412a2f94e2a3f81f10398f9c88a6f2292e
Stuart Terrett <shterrett@gmail.com>
2020-08-05 13:35:37 -0400

    Read json from stdin and pretty print

--- jl/app/Main.hs
+++ jl/app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
-import Lib
+import JL.PrettyPrint
+
+import qualified Data.Aeson as A
+import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as BS
+import System.Exit (exitSuccess, exitFailure)
 
 main :: IO ()
-main = someFunc
+main = do
+  f <- BS.getContents
+  case A.eitherDecode f of
+    Left e -> do
+      putStrLn e
+      exitFailure
+    Right json -> do
+      putStrLn $ T.unpack $ prettyPrint "  " json
+      exitSuccess
