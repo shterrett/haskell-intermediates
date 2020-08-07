0b1834e7e532896bf03b4e37fda90822f9348dfd
Stuart Terrett <shterrett@gmail.com>
2020-08-07 09:57:43 -0400

    Integrate focusing into main

--- jl/app/Main.hs
+++ jl/app/Main.hs
@@ -1,19 +1,42 @@
 module Main where
 
-import JL.PrettyPrint
+import JL.PrettyPrint (prettyPrint)
+import JL.Focus.Parse (formatFocus, parseFocus)
+import JL.Focus (focusOn)
 
 import qualified Data.Aeson as A
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as BS
-import System.Exit (exitSuccess, exitFailure)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
 
 main :: IO ()
 main = do
-  f <- BS.getContents
-  case A.eitherDecode f of
-    Left e -> do
-      putStrLn e
-      exitFailure
-    Right json -> do
-      putStrLn $ T.unpack $ prettyPrint "  " json
-      exitSuccess
+  input <- BS.getContents
+  json :: A.Value <- case A.eitherDecode input of
+                        Left e -> do
+                          putStrLn e
+                          exitFailure
+                        Right j -> pure j
+  args <- getArgs
+  focusString <- case args of
+                    [s] -> pure $ Just s
+                    [] -> pure Nothing
+                    _ -> do
+                      putStrLn $ "Provide zero or one argument"
+                      exitFailure
+  focusedJson <- case focusString of
+                    Nothing -> pure $ Right ([], json)
+                    Just fs -> case parseFocus (T.pack fs) of
+                                Left e -> do
+                                  putStrLn $ T.unpack e
+                                  exitFailure
+                                Right foci -> pure (focusOn foci json)
+  case focusedJson of
+    Right (_, fj) -> putStrLn . T.unpack $ prettyPrint "  " fj
+    Left (matched, fj) -> do
+      putStrLn . T.unpack 
+        $ "Only a partial match on the focus string was possible\n"
+       <> "The matched prefix was: " <> foldMap formatFocus matched <> "\n"
+       <> "and the matched object was:\n"
+       <> prettyPrint "  " fj
