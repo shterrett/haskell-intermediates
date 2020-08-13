92e2b78c82eef02bac83ffae8921de8663d57f66
Stuart Terrett <shterrett@gmail.com>
2020-08-12 20:58:59 -0400

    Add parsing haddocks

--- jl/src/JL/Focus.hs
+++ jl/src/JL/Focus.hs
@@ -1,3 +1,13 @@
+-- | Description: building lens into the json
+--
+-- This module handles the list of focuses returned from parsing the focus
+-- string
+-- The list is transformed into a list of accessors, and then these are composed
+-- together. Each @init@ of this list is run independently, and the longest one
+-- to succeed is reported. If the full list succeeds, then the focus was a
+-- success. Otherwise, it was a failure, and the longest successful focus is
+-- returned.
+
 module JL.Focus where
 
 import JL.Focus.Parse (Focus(..))
@@ -10,17 +20,28 @@ import qualified Data.Aeson as A
 import qualified Data.Aeson.Lens as A
 import qualified Data.Vector as V
 
+-- | Uses the language of lens to represent each focus. A @Key@ or @AnItem@ will
+-- become a @Prism@, and thus will be called with @preview@. An @Array@ will
+-- become a @Fold@ and thus be called with @toListOf@
 data Accessor =
   Preview
   | ToListOf
   deriving (Show, Eq, Ord)
 
+-- | Each of the @init@s of the list (built by a scan) is a "level" of focusing.
+-- So, for a list of focuses '[Key "a", AnItem "b", Key "c"]', the first level
+-- will have only '[Key "a"]', the second '[Key "a", AnItem "b"]', and the third
+-- will have '[Key "a", AnItem "b", Key "c"]'. Each level can then be applied to
+-- the json and succeed or fail independently.
 data Level = Level
-  { levels :: [Focus]
-  , optic :: Fold A.Value A.Value
-  , accessor :: Accessor
+  { levels :: [Focus] -- ^ the list of focuses for error reporting
+  , optic :: Fold A.Value A.Value -- ^ the actual composed optic that will be applied to the json
+  , accessor :: Accessor -- ^ the way in which the optic will be called
   }
 
+-- | the main function in this module. Given a list of focuses and a json value,
+-- attempt to focus on the target. Returns @Right@ if the entire focus string
+-- succeeds; otherwise, returns @Left@ with the longest succeeding sub-path.
 focusOn :: NonEmpty Focus -> A.Value -> Either ([Focus], A.Value) ([Focus], A.Value)
 focusOn fs json =
   let
@@ -33,6 +54,9 @@ focusOn fs json =
           else Left $ NE.last neRs
       Nothing -> Left ([], json)
 
+-- | The actual call of the optic. Essentially, it is 'preview optic', but there
+-- is a bit of special logic to convert empty results from @toListOf@ to
+-- @Nothing@
 doFocus :: A.Value -> Level -> Maybe ([Focus], A.Value)
 doFocus json Level{..} =
   case accessor of
@@ -41,6 +65,8 @@ doFocus json Level{..} =
                   [] -> Nothing
                   as -> Just (levels, A.Array $ V.fromList as)
 
+-- | the @scanl@ builds a sequence of levels such that each one has one more
+-- focus than the last.
 buildOptics :: NonEmpty Focus -> [Level]
 buildOptics (f :| fs) =
   scanl buildOptic (buildOptic emptyLevel f) fs
--- jl/src/JL/Focus/Parse.hs
+++ jl/src/JL/Focus/Parse.hs
@@ -1,3 +1,5 @@
+-- | Description: parses the focus string into @[Focus}@ that can be processed
+-- by @JL.Focus@
 module JL.Focus.Parse where
 
 import Text.Parsec (parse, Parsec, many1, (<|>), try)
@@ -7,37 +9,50 @@ import qualified Data.List.NonEmpty as NE
 import Data.Text (Text)
 import qualified Data.Text as T
 
+-- | the result of parsing each segment of a string
 data Focus =
-  Key Text
-  | Array
-  | AnItem Int
+  Key Text -- ^ from a key ame
+  | Array -- ^ from [*]
+  | AnItem Int -- ^ from [key name]
   deriving (Show, Eq)
 
+-- | inverse of parsing, this returns the '[Focus]' to a focus string
+-- There is a quickcheck test to ensure that this and parsing are inverses
 formatFocus :: Focus -> Text
 formatFocus (Key k) = "." <> k
 formatFocus Array = "[*]"
 formatFocus (AnItem i) = "[" <> T.pack (show i) <> "]"
 
+-- | the main function, it parses a focus string into '[Focus]'
+-- Given a string like firstKey.secondKey[*].thirdKey.[fourthKey]
+-- it returns '[Key "firstKey", Key "secondKey", Array, Key "thirdKey", AnItem "fourthKey"]'
 parseFocus :: Text -> Either Text (NonEmpty Focus)
 parseFocus t = case parse focusParser "" t of
                Left e -> Left $ T.pack $ show e
                Right fs -> maybe (Left "Parsed empty list of foci") Right
                             $ NE.nonEmpty fs
 
+-- The parser for the overall string. Returns zero or more of either key, array,
+-- or items
 focusParser :: Parsec Text () [Focus]
 focusParser = many1 (try keyParser
                       <|> try arrayParser
                       <|> try itemParser
                     )
 
+-- | parses '".name"' to 'Key "name"'
 keyParser :: Parsec Text () Focus
 keyParser = Key <$> (char '.' *> textParser)
 
+-- | parses '"[*]"' to 'Array'
 arrayParser :: Parsec Text () Focus
 arrayParser = string "[*]" *> pure Array
 
+-- | parses '"[name]"' to 'AnItem "name"'
 itemParser :: Parsec Text () Focus
 itemParser = AnItem . read <$> (char '[' *> many1 digit <* char ']')
 
+-- | basic definition of allowed text. This is probably too simple, but is
+-- generally ok.
 textParser :: Parsec Text () Text
 textParser = T.pack <$> many1 (noneOf ".[")
